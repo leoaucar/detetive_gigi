e42ec6aba25c3433c03060e643c8ced45cf00080
leoaucar <leonardoaucar@gmail.com>
2024-12-07 19:58:42 -0300

    change place functionality added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const changePlaceButton = document.getElementById('change-place')
+
+
 const itens_list = [
     Notebook = {
         name: "Notebook",
@@ -15,32 +18,48 @@ const character_list = [
     }
 ]
 
-
 const places = [
     {
-        name:"Granny house",
-        characters: null,
+        name:"Office",
+        characters: "Gigi",
         itens: null,
-        description:"Granny's house smelt like catnip"
+        description:"Crime never sleeps. We do.",
+        context_image: 'images\\gigi_capa.jpg'
     },
     {
         name: "Police Station",
         characters: character_list[0].name,
         itens: itens_list[0],
-        description: "Just an ordinary police station"
-    }
+        description: "Just an ordinary police station.",
+        context_image: 'images\\grafite_police_station.jpg'
+    },
+    {
+        name:"Granny house",
+        characters: "None alive",
+        itens: null,
+        description:"Granny's house smelt like catnip.",
+        context_image: "images\\nanquim_granny.jpg"
+    },
+
 ]
-let currentPlace = places[1]
+let currentPlace = places[0]
 
 function update_place(){
-    let currentPlaceText = currentPlace.description
-    console.log(currentPlaceText)
+    placeIndex = Number(document.getElementById('places-dropdown').value);
+    currentPlace = places[placeIndex]
+    const currentPlaceText = currentPlace.name
+    const placeDescription = currentPlace.description
+    const characters = currentPlace.characters
     document.getElementById('current-place-text').textContent = currentPlaceText
+    document.getElementById('place-description').textContent = `${placeDescription} ${characters} is here.`
+    document.getElementById('context-image').src = currentPlace.context_image
+    
 }
 
 function list_characters(place){
 }
 
+update_place()
 console.log("You are at the ", currentPlace.name)
 console.log(currentPlace.characters, "is in the scene")
-update_place()
+changePlaceButton.addEventListener("click", update_place);
